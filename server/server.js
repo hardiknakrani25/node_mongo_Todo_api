5b6abd5788db9735b2f977653d5c1990e2493da3
Hardik Nakrani <hdnakarani@gmail.com>
2018-12-16 09:33:47 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -26,10 +26,18 @@ text:req.body.text
     });
 
 })
-
-
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+      res.send({todos});
+    }, (e) => {
+      res.status(400).send(e);
+    })
+  });
 
 app.listen(3000,()=>{
     console.log(`Started on port 3000`);
     
 })
+
+
+module.exports={app};
